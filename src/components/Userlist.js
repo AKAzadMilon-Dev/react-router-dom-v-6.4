77f9377635a15cfbdc0ba8f0e47776df79dd922b
AK Azad Milon <akazadmilon.dev@gmail.com>
2022-11-23 20:39:22 +0600

    api-axios-get-data-react-router-dom-process

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { getUser } from "../api/api";
+import { useLoaderData } from "react-router-dom";
 
 const Userlist = () => {
-  return (
-    <div>Userlist</div>
-  )
-}
+  let data = useLoaderData();
+  console.log(data.data);
 
-export default Userlist
+  return (
+    <div>
+      {data.data.map((item, index) => (
+        <>
+          <h3>
+            {index + 1}.{item.name}
+          </h3>
+          <p>{item.username}</p>
+          <p>{item.email}</p>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Userlist;
+
+export function user() {
+  return getUser();
+}
